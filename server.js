3b09396c941ddead4941a5e6f63b8c5b9b2429d9
steve_D <stephenCdoherty@gmail.com>
2023-11-16 17:00:08 -0600

    server set up

--- server.js
+++ server.js
@@ -1,8 +1,42 @@
 const express =  require('express')
+const morgan = require("morgan");
+require('dotenv').config();
+const helmet = require('helmet');
+const OpenAI = require('openai');
+
+
+const PORT = process.env.PORT;
 const app = express()
+// const { Configuration, OpenAIApi } = require("openai")
+// import OpenAI from 'openai';
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY 
+});
+
 
 app.use(express.static('public'))
+app.use(morgan("dev"));
+// Use helmet middleware with the desired referrerPolicy
+app.use(helmet({
+  referrerPolicy: { policy: 'same-origin' } // Set to 'same-origin' or other desired policy
+}));
 
-app.listen(5000, ()=> {
+app.post('/chat', async (req, res)=> {   
+  try {
+    const resp = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+        messages: [
+          { role: "user", content: "Hello"}
+        ]  
+    })           
+        
+    res.status(200).json({message: resp.data.choices[0].message.content})
+  } catch(e) {
+      res.status(400).json({message: e.message})
+  }
+})
+
+app.listen(PORT, ()=> {
     console.log("Server is active")
 })
+
