2efe489112f2b5c72048a0607ab557de9cc7aa35
steve_D <stephenCdoherty@gmail.com>
2023-11-22 18:53:26 -0600

    error check

--- server.js
+++ server.js
@@ -30,9 +30,10 @@ app.post('/chat', async (req, res)=> {
           { role: "user", content: "req.body"}
         ]  
     })           
-        
+
     res.status(200).json({message: resp.data.choices[0].message.content})
   } catch(e) {
+            console.log(e)
       res.status(400).json({message: e.message})
   }
 })
